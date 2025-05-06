2265531aa46cbe21bce1059052529942fa0e6e92
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-05-05 21:54:47 -0300

    004_delegates_as_method_parameters

---
+++ 004_delegates_as_method_parameters/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Delegates
+{
+  delegate void MessagePrinter(string message);
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      MessagePrinter printer = SurroundWithHyphens;
+      printer += SurroundWithStars; // Add another method to the delegate
+
+      MethodInvoker(printer, "Hello, World!");
+    }
+
+
+    // The method accepts a delegate of type MessagePrinter
+    static void MethodInvoker(MessagePrinter printer, string message)
+    {
+      // We normally use ? because the delegate can be null
+      printer?.Invoke(message);
+    }
+
+    static void SurroundWithStars(string message)
+    {
+      Console.WriteLine($"***{message}***");
+    }
+
+    static void SurroundWithHyphens(string message)
+    {
+      Console.WriteLine($"---{message}---");
+    }
+  }
+}
