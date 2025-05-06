e0a3140476f3b3451d0c0f8834952b2ee9e8db2a
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-05-05 21:54:40 -0300

    003_delegates_combine

---
+++ 003_delegates_combine/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Delegates
+{
+  delegate void MessagePrinter(string message);
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Create delegates of the same type
+      MessagePrinter messagePrinter1 = SurroundWithHyphens;
+      MessagePrinter messagePrinter2 = SurroundWithStars;
+      MessagePrinter messagePrinter3 = SurroundWithStars;
+
+      // combine the delegates
+      MessagePrinter combinedPrinter = messagePrinter1 + messagePrinter2 + messagePrinter3;
+
+      // Call the combined delegate
+      combinedPrinter("Hello, World!");
+    }
+    static void SurroundWithStars(string message)
+    {
+      Console.WriteLine($"***{message}***");
+    }
+
+    static void SurroundWithHyphens(string message)
+    {
+      Console.WriteLine($"---{message}---");
+    }
+  }
+
+
+}
