3509a83e2e153a9d2d1a9f2d62c763d44c8bc492
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-05-05 22:06:20 -0300

    005_delegates_generic

---
+++ 005_delegates_generic/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Delegates
+{
+
+  class Program
+  {
+    // Generic delegate
+    // delegate T2 means, that the return type of the delegate is T2,
+    delegate T2 MathOperator<T1, T2>(T1 operador1, T1 operador2);
+
+    static void Main(string[] args)
+    {
+      // Instantiating a generic delegate
+      MathOperator<int, long> operator1 = Adder;
+      MathOperator<int, double> operator2 = Divider;
+
+      long sum = operator1(10, 20);
+      double divisionResult = operator2(7, 2);
+
+      Console.WriteLine(sum);
+      Console.WriteLine(divisionResult);
+    }
+
+    static long Adder(int firstNumber, int secondNumber)
+    {
+      return firstNumber + secondNumber;
+    }
+
+    static double Divider(int firstNumber, int secondNumber)
+    {
+      return (double)firstNumber / secondNumber;
+    }
+  }
+}
