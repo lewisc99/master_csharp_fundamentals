7a16cb1c584e13a59c3ce0f511ecebcbda8623ba
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-05-06 22:23:10 -0300

    006_delegates_anonymous_methods/

---
+++ 006_delegates_anonymous_methods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AnonymousMethods
+{
+
+  class Program
+  {
+    delegate void MessagePrinter();
+
+    static void Main(string[] args)
+    {
+      // This delegate will have the reference
+      // of the anonymous method 
+      // anonymous method is basically a method without a name
+      // and without a return type
+      MessagePrinter printer = delegate ()
+      {
+        Console.WriteLine("Hello, World!");
+      };
+
+      printer(); // Call the delegate to execute the anonymous method
+    }
+  }
+}
