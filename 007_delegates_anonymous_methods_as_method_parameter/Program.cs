6d930d5ded05c5744a35f223b60d8c7095049602
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-05-08 22:43:27 -0300

    007_delegates_anonymous_methods_as_method_parameter

--- 007_delegates_anonymous_methods_as_method_parameter/Program.cs
+++ 007_delegates_anonymous_methods_as_method_parameter/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace AnonymousMethods
+{
+
+  // Our delegate
+  delegate void NotificationHandler(string notification);
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        // Instead of creating an instance of the delegate, 
+        // we can use an anonymous method directly in the method call
+        NotifyUser("You have a new message!", delegate (string notification) {
+            Console.WriteLine(notification);
+        });
+    }
+
+    // This method accepts a delegate as a parameter
+    static void NotifyUser(string notification, NotificationHandler handler)
+    {
+      // Call the delegate
+      handler(notification);
+    }
+  }
+}
