9cb0043a82c4ab7d6d8a6a87106737031d20bca9
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-05-09 22:39:22 -0300

    009_delegates_anonymous_method_with_no_parameters

---
+++ 009_delegates_anonymous_method_with_no_parameters/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AnonymousMethods
+{
+
+  // Return type is void, accepts one parameter
+  delegate void NotificationHandler(string notification);
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // No parameters, even though the delegate accepts a string object
+      // as a parameter, this delegate could be used to pass a string object
+      NotifyUser("Battery low", delegate
+      {
+        // The anonymous method does not return anything (void)
+        Console.WriteLine("Notification received");
+      });
+
+      // another example - with parameter
+      NotifyUser("Battery low - with parameter", delegate (string notification)
+      {
+        // The anonymous method does not return anything (void)
+        Console.WriteLine(notification);
+      });
+    }
+
+    static void NotifyUser(string notification, NotificationHandler handler)
+    {
+      // Call the delegate
+      handler(notification);
+    }
+  }
+}
