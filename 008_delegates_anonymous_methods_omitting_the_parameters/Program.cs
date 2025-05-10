c67665b7f2b5f1c93c176b7204cacd2214e0b0f6
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-05-09 22:26:25 -0300

    008_delegates_anonymous_methods_omitting_the_parameters

---
+++ 008_delegates_anonymous_methods_omitting_the_parameters/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AnonymousMethods
+{
+  delegate void MessagePrinter();
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Since the anonymous method doest not accept any parameters, we can omit the parameter list
+      MessagePrinter printer = delegate
+      {
+        Console.WriteLine("Hello, World!");
+      };
+
+      // Call the delegate
+      printer();
+    }
+  }
+}
