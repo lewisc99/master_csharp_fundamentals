2fc1f31e09f2eac36e8b28fd4984d539893a0fe2
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-05-09 22:53:28 -0300

    011_delegates_anonymous_method_accessing_variables

---
+++ 011_delegates_anonymous_method_accessing_variables/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AnonymousMethods
+{
+  delegate void MathOperation();
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Variables defined inside the Main method
+      int number1 = 61;
+      int number2 = 88;
+
+      // Our anonymous method can access variables defined
+      // in the context where the anonymous method is created
+      MathOperation mathOperation = delegate
+      {
+        // Can access number1 and number2
+        Console.WriteLine(number1 + number2);
+      };
+
+      mathOperation();
+    }
+  }
+}
