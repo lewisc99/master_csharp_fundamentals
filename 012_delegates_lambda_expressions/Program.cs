0301597f09928df9ce26f9f7271512877f210063
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-05-10 22:41:07 -0300

    012_delegates_lambda_expressions

---
+++ 012_delegates_lambda_expressions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lambdas
+{
+  delegate int MathOperation(int operand1, int operand2);
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Lambda expression
+      MathOperation mathOperation = (operand1, operand2) => operand1 + operand2;
+
+      Console.WriteLine(mathOperation(10, 5)); // Output: 15
+
+      // Lambda expression
+      mathOperation += (operand1, operand2) => operand1 - operand2;
+
+      Console.WriteLine(mathOperation(10, 5)); // Output: 5
+    }
+  }
+}
