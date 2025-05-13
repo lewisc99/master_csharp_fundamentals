77196bef71bc53873c4986fc98fb7fcabf4ad504
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-05-12 22:50:28 -0300

    013_delegates_lambda_single_parameter

---
+++ 013_delegates_lambda_single_parameter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lambdas
+{
+
+  delegate int IncrementOperation(int number);
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // No parantheses became there is only one parameter
+      IncrementOperation increment = number => number + 1;
+
+      Console.WriteLine(increment(5)); // Output: 6
+    }
+  }
+}
