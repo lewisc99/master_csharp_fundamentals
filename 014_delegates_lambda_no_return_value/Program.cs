cf8fd1ac6ed10386e68a3cfbe0a0944fd5f2a5e4
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-05-12 22:50:36 -0300

    014_delegates_lambda_no_return_value

---
+++ 014_delegates_lambda_no_return_value/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lambdas
+{
+
+  // Does not return anything
+  // Does not accept any parameters
+  delegate void MessagePrinter();
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Empty parantheses if no parameters
+      // No return value
+
+      MessagePrinter printer = () => Console.WriteLine("Hello, World!");
+      printer += () => Console.WriteLine("Hello, Again!");
+
+      // if you not add printer() to the end of the line, it will not execute
+      // any of the methods
+      printer(); // Output: Hello, World! Hello, Again!
+    }
+  }
+}
