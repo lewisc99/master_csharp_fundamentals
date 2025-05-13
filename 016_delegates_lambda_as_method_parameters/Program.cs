c96d39a6c82579efb32002c8114c6b8a920a0ce9
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-05-12 22:50:57 -0300

    016_delegates_lambda_as_method_parameters

---
+++ 016_delegates_lambda_as_method_parameters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lambdas
+{
+
+  delegate void IncrementOperation(ref int number);
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 10;
+
+      ModifyNumber(ref x, (ref int number) => number++); // Output: 11
+      ModifyNumber(ref x, (ref int number) => number += 5); // Output: 16
+      ModifyNumber(ref x, (ref int number) => number *= 2); // Output: 32
+      ModifyNumber(ref x, (ref int number) => number -= 10); // Output: 22
+
+      Console.WriteLine(x); // Output: 22 because the last operation was to subtract 10 from 32
+    }
+
+    static void ModifyNumber(ref int number, IncrementOperation incrementor)
+    {
+      incrementor(ref number);
+    }
+  }
+}
