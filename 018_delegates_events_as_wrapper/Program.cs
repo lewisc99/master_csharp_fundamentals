56f7a3729054c5ed577542fcc83e06680f5f1e98
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-05-16 19:27:49 -0300

    018_delegates_events_as_wrapper

--- 018_delegates_events_as_wrapper/Program.cs
+++ 018_delegates_events_as_wrapper/Program.cs
@@ -11,10 +11,11 @@ namespace Delegates
       // We can only attach a method to the event
       // or unregister it
       // No other actions are possible
-      footballMatch.GoalScored += (scoringTeam, team1Score, team2Score) =>
-      {
-        Console.WriteLine($"{scorer} scored. Current score is {team1Score}:{team2Score}.");
-      };
+      footballMatch.GoalScored += (scorer, team1Score, team2Score) =>
+           {
+             Console.WriteLine($"{scorer} scored. Current score is {team1Score}:{team2Score}.");
+           };
+
 
       footballMatch.IncreaseTeam1Score();
       footballMatch.IncreaseTeam1Score();
