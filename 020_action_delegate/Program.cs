9c893f33c29cf2a97df0630e27dfd47c6b7aed0a
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-05-18 15:08:07 -0300

    020_action_delegate

---
+++ 020_action_delegate/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Delegates
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Accept an integer as a parameter
+      Action<int> incrementor;
+
+
+      incrementor = (x) => Console.WriteLine(x + 1);
+      incrementor += (x) => Console.WriteLine(x + 7);
+
+      incrementor(3);
+    }
+  }
+}
