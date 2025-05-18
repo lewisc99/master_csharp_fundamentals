355c971620973b6407dae58fbf746f97e04346b8
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-05-18 15:08:15 -0300

    021_overloaded_action_action_delegate

---
+++ 021_overloaded_action_action_delegate/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Delegates
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Accepts two integers as parameters.
+      // We can instantiate a delegate with up to 16 parameters.
+      // Action<int, string, bool, int, int> ...
+      Action<int, int> mathOperation;
+
+      mathOperation = (x, y) => Console.WriteLine(x + y);
+      mathOperation += (x, y) => Console.WriteLine(x * y);
+
+      mathOperation(3, 4);
+    }
+  }
+}
