9a29074551cc2ca90e712a9ac0c612a732e55309
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-05-18 15:08:32 -0300

    023_predicate_delegate

---
+++ 023_predicate_delegate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Delegates
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Predicate<int> isEven = delegate (int x)
+          {
+            return x % 2 == 0;
+          };
+
+      // Predicate returns a bool value
+      bool sevenIsEven = isEven(7);
+
+      if (sevenIsEven)
+      {
+        Console.WriteLine("7 is even");
+      }
+      else
+      {
+        Console.WriteLine("7 is odd");
+      }
+    }
+  }
+}
