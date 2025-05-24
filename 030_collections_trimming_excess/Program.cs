8a72f214d56d6d42950d5e3b6b07e0031d695626
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-05-24 18:19:13 -0300

    030_collections_trimming_excess

---
+++ 030_collections_trimming_excess/Program.cs
@@ -0,0 +1,21 @@
+﻿var numbers = new List<int>();
+Console.WriteLine(numbers.Capacity); // 0
+
+numbers.Add(17);
+Console.WriteLine(numbers.Capacity); // 4
+
+// Trimming the excess capacity
+// This will reduce the capacity to the number of elements in the list
+// In this case, it will be 1 since we have added only one element
+numbers.TrimExcess();
+Console.WriteLine(numbers.Capacity); // 1
+
+// Adding more elements
+numbers.Add(42);
+
+// The capacity will increase again
+// The new capacity will be the next power of 2 instead of 8,
+// Because without trimming, the capacity is multiplied by 2.
+// And each element is 4 bytes
+// So the new capacity will be 2 * 4 = 8 without trimming
+Console.WriteLine(numbers.Capacity); // 2
