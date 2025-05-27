294faad284ac76aa022e9446440b48ecd7d3dc4c
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-05-26 22:04:31 -0300

    032_collections_using_queue

---
+++ 032_collections_using_queue/Program.cs
@@ -0,0 +1,32 @@
+﻿// Let's add these people to our store queue
+var customer1 = new Person() { Name = "Anna" };
+var customer2 = new Person() { Name = "Patrick" };
+var customer3 = new Person() { Name = "John" };
+var customer4 = new Person() { Name = "Lily" };
+
+// Queue<T>: T can be any type
+var storeQueue = new Queue<Person>();
+storeQueue.Enqueue(customer1);
+storeQueue.Enqueue(customer2);
+storeQueue.Enqueue(customer3);
+storeQueue.Enqueue(customer4);
+
+// All customers in the queue
+Console.WriteLine("Customers in the queue:");
+foreach (var customer in storeQueue)
+{
+  // shows a list of customers in the queue
+  Console.WriteLine($"- {customer.Name}");
+}
+
+// Peek at the first customer in line
+while (storeQueue.Count > 0)
+{
+  Console.WriteLine($"Next customer in line is {storeQueue.Peek().Name}.");
+  Console.WriteLine($"{storeQueue.Dequeue().Name} paid and left.");
+}
+
+class Person
+{
+  public string Name { get; set; }
+}
