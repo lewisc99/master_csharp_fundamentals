bd66d082e9294c60c501ca630052dee86e5bd2e7
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-05-26 21:52:50 -0300

    031_collections_stacks

--- 031_collections_stacks/Program.cs
+++ 031_collections_stacks/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var stackOfInts = new Stack<int>();
+
+stackOfInts.Push(42);
+PrintLastPushed<int>(stackOfInts);
+PrintStackCount<int>(stackOfInts);
+
+stackOfInts.Push(68);
+PrintLastPushed<int>(stackOfInts);
+PrintStackCount<int>(stackOfInts);
+
+stackOfInts.Push(30);
+PrintLastPushed<int>(stackOfInts);
+PrintStackCount<int>(stackOfInts);
+
+// 30 is the last pushed item, // so it will be the first one popped
+Console.WriteLine("Popping the last item from the stack...");
+var lastPopped = stackOfInts.Pop();
+Console.WriteLine($"Popped {lastPopped} from the stack.");
+PrintStackCount<int>(stackOfInts);
+
+
+static void PrintLastPushed<T>(Stack<T> stack)
+{
+  // Peek reads without removing
+  Console.WriteLine($"Placed {stack.Peek()} at the top of the stack.");
+}
+
+static void PrintStackCount<T>(Stack<T> stack)
+{
+  Console.WriteLine($"There are {stack.Count} items in the stack.");
+}
