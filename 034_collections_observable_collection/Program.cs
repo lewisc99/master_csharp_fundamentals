6d90f1dee876547e39d7f71d9e0510b95f2229ad
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-05-28 22:37:59 -0300

    034_collections_observable_collection

---
+++ 034_collections_observable_collection/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+// ObservableCollection<T> is in this namespace
+using System.Collections.ObjectModel;
+
+var notes = new ObservableCollection<string>();
+notes.Add("I have to do the laundry today");
+
+Console.WriteLine(notes.Count); // Output: 1
