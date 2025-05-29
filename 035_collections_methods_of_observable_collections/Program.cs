036fdd828ad2ce9b3e826c6d9bc780b451c49942
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-05-29 20:39:31 -0300

    035_collections_methods_of_observable_collections

---
+++ 035_collections_methods_of_observable_collections/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+
+// ObservableCollection<T> is in this namespace
+using System.Collections.ObjectModel;
+
+var notes = new ObservableCollection<string>();
+notes.Add("I have to do the laundry today"); // Add a note
+
+notes.Add("Good day today"); // Add another note
+notes.RemoveAt(0); // Remove the first item
+notes.Insert(0, "I have to do the dishes today"); // Insert at the first position
+
+Console.WriteLine(notes.Count); // Output: 2
+
+
+// iterates through the collection
+foreach (var note in notes)
+{
+  // Output:
+  // I have to do the dishes today
+  // Good day today
+  Console.WriteLine(note);
+}
+
+// Count the number of notes
+Console.WriteLine(notes.Count);
