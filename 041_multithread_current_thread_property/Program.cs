c94508776e6d457173e572d73a1052359e5cbf38
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-06-07 20:37:05 -0300

    041_multithread_current_thread_property

---
+++ 041_multithread_current_thread_property/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading;
+
+// Gets the thread that is currently running
+var currentThread = Thread.CurrentThread;
+
+Console.WriteLine($"Current Thread ID: {currentThread.ManagedThreadId}");
+Console.WriteLine($"Current Thread Name: {currentThread.Name ?? "No Name"}");
+
+currentThread.Name = "MainThread";
+Console.WriteLine($"Current Thread Name after setting: {currentThread.Name}");
+
