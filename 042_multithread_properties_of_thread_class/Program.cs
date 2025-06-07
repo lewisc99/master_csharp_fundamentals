a3fb34ef51fed5b2fe29e2a051de409abcd8a22f
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-06-07 20:40:00 -0300

    042_multithread_properties_of_thread_class

---
+++ 042_multithread_properties_of_thread_class/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+// Demonstrates the properties of the Thread class
+var thread = Thread.CurrentThread;
+thread.Name = "MainThread";
+Console.WriteLine($"Thread ID: {thread.ManagedThreadId}");
+Console.WriteLine($"Thread Name: {thread.Name}");
+Console.WriteLine($"Is Thread Pool Thread: {thread.IsThreadPoolThread}");
+Console.WriteLine($"Thread State: {thread.ThreadState}");
+Console.WriteLine($"Priority: {thread.Priority}");
+Console.WriteLine($"Is Background Thread: {thread.IsBackground}");
+Console.WriteLine($"Thread Culture: {thread.CurrentCulture}");
+Console.WriteLine($"Thread Execution Context: {thread.ExecutionContext}");
+
+Console.WriteLine($"Thread Apartment State: {thread.GetApartmentState()}");
+Console.WriteLine($"Thread Is Alive: {thread.IsAlive}");
