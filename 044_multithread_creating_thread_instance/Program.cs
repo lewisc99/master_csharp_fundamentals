9e386fa098ae537a81fba3155e3534031c9f0b39
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-06-08 19:10:24 -0300

    044_multithread_creating_thread_instance

---
+++ 044_multithread_creating_thread_instance/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+// The are three ways t pass a parameterless method to a thread constructor:
+
+// 1. Create a ThreadStart delegate object and add a method to this delegate
+var threadStart = new ThreadStart(PrintHelloWorld);
+var thread = new Thread(threadStart);
+
+// 2. Pass a method directly to the Thread constructor
+var thread2 = new Thread(PrintHelloWorld);
+
+// 3. Use a lambda expression to pass a method to the Thread constructor
+var thread3 = new Thread(() => PrintHelloWorld());
+
+// 4. Pass an anonymous method that conforms to the ThreadStart delegate
+var thread4 = new Thread(() =>
+{
+  Console.WriteLine("4 Thread - Hello from an anonymous method!");
+});
+
+
+static void PrintHelloWorld()
+{
+  Console.WriteLine("Hello, World!");
+}
