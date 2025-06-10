93409ffd51cdaa24fd1b22abf6b2933ee8c0691d
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-06-09 21:38:22 -0300

    045_multithread_getting_thread_state

---
+++ 045_multithread_getting_thread_state/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+var threadStart = new ThreadStart(PrintHelloWorld);
+
+var thread = new Thread(threadStart);
+
+var thread2 = new Thread(PrintHelloWorld);
+
+var thread3 = new Thread(() => Console.WriteLine("Hello from anonymous method!"));
+
+
+// ThreadState property stores the state of the thread
+Console.WriteLine(thread.ThreadState);
+
+static void PrintHelloWorld()
+{
+  Console.WriteLine("Hello, World!");
+}
