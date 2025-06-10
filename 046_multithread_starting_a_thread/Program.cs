f8f02343ff8fd7749e561d8f80f8caf508920527
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-06-09 21:38:31 -0300

    046_multithread_starting_a_thread

---
+++ 046_multithread_starting_a_thread/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+var threadStart = new ThreadStart(PrintHelloWorld);
+
+var thread = new Thread(threadStart);
+
+var thread2 = new Thread(PrintHelloWorld);
+
+var thread3 = new Thread(() => Console.WriteLine("Hello from anonymous method!"));
+
+// Start the threads
+thread.Start();
+thread2.Start();
+thread3.Start();
+
+
+static void PrintHelloWorld()
+{
+  Console.WriteLine("Hello World!");
+}
