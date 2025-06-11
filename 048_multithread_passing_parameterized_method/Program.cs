457c36a1b8ed89b223b3a7b23fbc3955c9bb52d8
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-06-10 21:56:25 -0300

    048_multithread_passing_parameterized_method

---
+++ 048_multithread_passing_parameterized_method/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+// There are three ways to pass parameters to a method in a thread:
+
+// 1. Create a ParameterizedThreadStart delegate object and add a method to this delegate
+var parameterizedThreadStart = new ParameterizedThreadStart(PrintNumbers);
+var thread = new Thread(parameterizedThreadStart);
+
+// 2. Pass a method name directly
+var thread2 = new Thread(PrintMessage);
+
+// 3. Pass an anonymous method that conforms to the ParameterizedThreadStart delegate
+var thread3 = new Thread((message) =>
+        {
+          Console.WriteLine(message);
+        });
+
+// PrintMessage() method has the same signature and return type as ParameterizedThreadStart delegate
+static void PrintMessage(object message)
+{
+  Console.WriteLine("Hello World!");
+}
