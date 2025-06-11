3b02164b51d943a96a315de6cb68df523b40ea88
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-06-10 21:56:40 -0300

    050_multithread_casting_an_item_of_type_object_to_expected

---
+++ 050_multithread_casting_an_item_of_type_object_to_expected/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+var thread = new Thread(Divider);
+var thread2 = new Thread(Divider);
+
+// We pass an int value to the thread
+thread.Start(5623);
+
+// Our method accepts a parameter of type object
+static void Divider(object number)
+{
+  // We want double or int items to be passed.
+  // We check before making any operations
+  if (number is double || number is int)
+  {
+    // If the parameter is of type double or int,
+    // we perform the calculation
+    var result = Convert.ToDouble(number) / 2;
+    Console.WriteLine(result);
+  }
+  else
+  {
+    Console.WriteLine("Parameter must be of type int or double.");
+  }
+}
