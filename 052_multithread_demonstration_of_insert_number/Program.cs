510ec4e60b5fd0e20d82da803457649aac24e6c2
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-06-13 20:56:08 -0300

    052_multithread_demonstration_of_insert_number

---
+++ 052_multithread_demonstration_of_insert_number/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SynchronizingThreads
+{
+  class Program
+  {
+    static string text = "Microsoft";
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine($"Value of `text` before calling the InsertNumber() method: {text}.");
+      InsertNumber(10);
+      Console.WriteLine($"Value of `text` after calling the InsertNumber() method: {text}.");
+    }
+
+    static void InsertNumber(object number)
+    {
+      if (number is int)
+      {
+        string original = new string(text);
+        text = text.Insert(text.Length / 2, number.ToString());
+        Console.WriteLine(text);
+        text = original;
+      }
+    }
+  }
+}
