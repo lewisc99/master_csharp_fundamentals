96bde1f811ea98f0b2de8ce86b9e93efcf5b2f98
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-06-13 21:03:50 -0300

    053_multithread_several_threads_using_same_resource

---
+++ 053_multithread_several_threads_using_same_resource/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace SynchronizingThreads
+{
+  class Program
+  {
+    static string text = "Microsoft";
+
+    static void Main(string[] args)
+    {
+      var thread1 = new Thread(InsertNumber);
+      var thread2 = new Thread(InsertNumber);
+      var thread3 = new Thread(InsertNumber);
+
+      thread1.Start(1);
+      thread2.Start(2);
+      thread3.Start(3);
+    }
+
+    static void InsertNumber(object number)
+    {
+      if (number is int)
+      {
+        string original = new string(text);
+        text = text.Insert(text.Length / 2, number.ToString());
+        Console.WriteLine(text);
+        text = original;
+      }
+    }
+  }
+}
