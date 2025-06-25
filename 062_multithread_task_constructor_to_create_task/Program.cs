b715c2cce1cd170969a3542c370976dd6a3f66f8
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-06-24 21:11:25 -0300

    062_multithread_task_constructor_to_create_task

---
+++ 062_multithread_task_constructor_to_create_task/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+// We can pass a name of the method conforms to the Action delegate
+var task = new Task(Print);
+task.Start();
+
+// We can pass an anonymous method
+var task2 = new Task(() => { Console.WriteLine("Anonymous method inside a task"); });
+
+task2.Start();
+
+// As tasks run on thread pool threads,
+// A foreground thread must be running or else tasks will stop.
+Thread.Sleep(2000);
+
+static void Print()
+{
+  Console.WriteLine("Executing inside a task");
+}
