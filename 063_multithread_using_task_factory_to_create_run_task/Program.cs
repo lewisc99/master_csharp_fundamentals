b02abdbcf05324258152a009dbb9ffcab15b71a6
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-06-25 21:29:04 -0300

    063_multithread_using_task_factory_to_create_run_task

---
+++ 063_multithread_using_task_factory_to_create_run_task/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+var task = Task.Factory.StartNew(Print);
+var task2 = Task.Factory.StartNew(() => { Console.WriteLine("Anonymous method inside a task"); });
+
+// As tasks run on thread pool threads, 
+// a foreground thread must be running or else tasks will stop
+Thread.Sleep(2000);
+
+static void Print()
+{
+  Console.WriteLine("Executing inside a task");
+}
