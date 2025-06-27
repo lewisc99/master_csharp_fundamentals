89efbe9e45be66fab8ed2325d692b02806df5493
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-06-26 21:16:30 -0300

    064_multithread_task_using_run

---
+++ 064_multithread_task_using_run/Program.cs
@@ -0,0 +1,12 @@
+﻿
+var task = Task.Run(print);
+var task2 = Task.Run(() => Console.WriteLine("Anonymous method inside a task"));
+
+// As tasks run on thread pool threads, 
+// a foreground thread must be running or else tasks will stop
+Thread.Sleep(2000);
+
+static void print()
+{
+  Console.WriteLine("Executing inside a task");
+}
