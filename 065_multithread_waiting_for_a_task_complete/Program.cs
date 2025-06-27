0d1f99a72e4e8a7950f132a050af50cb1c87b514
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-06-26 21:21:15 -0300

    065_multithread_waiting_for_a_task_complete

---
+++ 065_multithread_waiting_for_a_task_complete/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+var task = Task.Run(Print);
+var task2 = Task.Run(() => { Console.WriteLine("Anonymous method inside a task"); });
+
+task.Wait();
+task2.Wait();
+
+
+static void Print()
+{
+  Console.WriteLine("Executing inside a task");
+}
