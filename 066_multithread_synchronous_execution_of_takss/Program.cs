2a0085fdefbba622951911376d7da00b7338fb79
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-06-26 21:38:08 -0300

    066_multithread_synchronous_execution_of_takss

---
+++ 066_multithread_synchronous_execution_of_takss/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CreatingTasks
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var task = new Task(Print);
+      var task2 = new Task(() => { Console.WriteLine("Anonymous method inside a task"); });
+
+      task.RunSynchronously();
+
+      // Waits until the first task finishes
+      task2.RunSynchronously();
+
+      // The Main() method does not end until task2 finishes
+    }
+
+    static void Print()
+    {
+      Console.WriteLine("Executing inside a task");
+    }
+  }
+}
