039bf75d3149c2fe493eca1618968044ac32768d
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-06-29 20:05:19 -0300

    068_multithread_running_several_methods_in_parallel_using_invoke

---
+++ 068_multithread_running_several_methods_in_parallel_using_invoke/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+Parallel.Invoke(PrintFirst, PrintSecond, PrintThird);
+
+static void PrintFirst()
+{
+  Console.WriteLine("Started to run first.");
+}
+
+static void PrintSecond()
+{
+  Console.WriteLine("Started to run second.");
+}
+
+static void PrintThird()
+{
+  Console.WriteLine("Started to run third.");
+}
