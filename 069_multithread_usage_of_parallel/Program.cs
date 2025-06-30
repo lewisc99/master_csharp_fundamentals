207c6712de0a888ed587592b84abfc4bf940088f
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-06-30 20:38:20 -0300

    069_multithread_usage_of_parallel

---
+++ 069_multithread_usage_of_parallel/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+// We have files to convert
+string[] filesToConvert = new string[]
+{
+                "EmployeeList.docx",
+                "Salaries.docx",
+                "Rules.docx",
+                "EmploymentContract.docx",
+                "Handbook.docx",
+                "FAQ.docx",
+                "untitled.docx",
+                "New.docx",
+                "Graphs.docx",
+                "EOD.docx"
+};
+
+// Insted of using a for loop to convert each file 
+// We can convert in parallel using Parallel.For()
+Parallel.For(0, filesToConvert.Length, (i) =>
+{
+  Console.WriteLine($"Successfully converted file {filesToConvert[i]} to PDF.");
+
+});
