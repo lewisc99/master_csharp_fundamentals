26097b7a40c5e15cf3040afb1ba7bf1b655faa10
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-06-30 20:38:51 -0300

    070_multithread_parallel_foreach

---
+++ 070_multithread_parallel_foreach/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+// We have files to convert
+string[] filesToConvert = new string[]
+{
+                "EmployeeList.docx",
+                "Salaries.docx",
+                "Rules.docx",
+                "EmploymentContract.docx",
+                "Handbook.docx",
+                "FAQ.docx",
+                "untitled.docx",
+                "New.docx",
+                "Graphs.docx",
+                "EOD.docx"
+};
+
+// Instead of using a foreach loop to convert each file,
+// we can convert in parallel using Parallel.ForEach()
+Parallel.ForEach<string>(filesToConvert, (fileName) =>
+{
+  Console.WriteLine($"Successfully converted file {fileName} to PDF.");
+});
