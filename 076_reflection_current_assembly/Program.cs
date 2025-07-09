1bae64135b7be1a2939baabdf4bba79bae6e274e
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-07-08 21:03:55 -0300

    076_reflection_current_assembly

---
+++ 076_reflection_current_assembly/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Reflection;
+
+// The Program class is the one we are working on
+Type typeInfoOnProgram = typeof(Program);
+
+Assembly programAssembly = typeInfoOnProgram.Assembly;
+AssemblyName programAssemblyName = programAssembly.GetName();
+
+Console.WriteLine($"Assembly name: {programAssemblyName.Name}");
+Console.WriteLine($"Assembly version: {programAssemblyName.Version}");
