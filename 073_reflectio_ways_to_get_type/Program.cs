3cc28152bc4a871be929ef0908f3129541c7dbab
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-07-08 21:03:29 -0300

    073_reflectio_ways_to_get_type

---
+++ 073_reflectio_ways_to_get_type/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+// 1st approach
+Type typeInfoOnInteger = typeof(int);
+Console.WriteLine(typeInfoOnInteger);
+
+// 2nd approach
+var stringObject = "Hello world!";
+Type typeInfoOnString = stringObject.GetType();
+Console.WriteLine(typeInfoOnString);
+
+//  3nd approach
+Type typeInfoOnBoolean = Type.GetType("System.Boolean");
+Console.WriteLine(typeInfoOnBoolean);
