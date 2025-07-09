e981656748f550c8a29d5109d7a9a1192583f2b7
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-07-08 21:03:45 -0300

    075_reflection_typeof_assembly

---
+++ 075_reflection_typeof_assembly/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+Type typeInfoOnString = typeof(string);
+
+Assembly stringAssembly = typeInfoOnString.Assembly;
+AssemblyName stringAssemblyName = stringAssembly.GetName();
+
+Console.WriteLine($"Assembly name: {stringAssemblyName.Name}");
+Console.WriteLine($"Assembly version: {stringAssemblyName.Version}");
