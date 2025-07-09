1e6932cf4eca0ccfc09338ebfe73df75a67d37e2
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-07-08 21:03:37 -0300

    074_reflection_information_on_system_string

---
+++ 074_reflection_information_on_system_string/Program.cs
@@ -0,0 +1,9 @@
+﻿Type typeInfoOnString = typeof(string);
+
+Console.WriteLine($"Name: {typeInfoOnString.Name}.");
+Console.WriteLine($"Full name: {typeInfoOnString.FullName}.");
+Console.WriteLine($"Namespace: {typeInfoOnString.Namespace}.");
+Console.WriteLine($"Base type: {typeInfoOnString.BaseType}.");
+Console.WriteLine($"Is class: {typeInfoOnString.IsClass}.");
+Console.WriteLine($"Assembly: {typeInfoOnString.Assembly}.");
+Console.WriteLine($"Attributes: {typeInfoOnString.Attributes}.");
