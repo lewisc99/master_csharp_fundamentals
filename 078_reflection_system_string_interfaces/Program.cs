273fbf0ccccb6b2a6140ea0e422f87bc5ee50cf1
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-07-11 22:41:25 -0300

    078_reflection_system_string_interfaces

---
+++ 078_reflection_system_string_interfaces/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+
+Type typeString = typeof(string);
+
+Type[] implementedInterfaces = typeString.GetInterfaces();
+
+Console.WriteLine($"{typeString.Name} implements {implementedInterfaces.Length} interfaces:");
+
+foreach (var type in implementedInterfaces)
+{
+  Console.WriteLine(type.Name);
+}
