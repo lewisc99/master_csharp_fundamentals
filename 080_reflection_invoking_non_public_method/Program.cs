b7e1d260e74635055f13962eb6b2895887fce59e
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-07-11 22:41:46 -0300

    080_reflection_invoking_non_public_method

---
+++ 080_reflection_invoking_non_public_method/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+
+namespace ReflectionIntroduction
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string fruits = "apple,banana,orange";
+      Type type = typeof(string);
+
+      // Look for non-public methods
+      MethodInfo[] methodInfos = type.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+      Console.WriteLine($"Non-public methods of {type.Name}:");
+
+      foreach (var methodInfo in methodInfos)
+      {
+        Console.WriteLine(methodInfo.Name);
+      }
+
+
+      // There's SplitInternal method, we will call it despite it's internal and not accessible by us
+      MethodInfo splitInternalMethod = methodInfos[3];
+      Console.WriteLine("\nCalling the SplitInternal() method..");
+
+      // We call the method using Invoke(). If it is an instance method, then the first parameter
+      // is an object on which the method will be called. We pass the parameters as an object array (object[])
+      var result = splitInternalMethod.Invoke(fruits, new object[] { ",", new string[] { "," }, Int32.MaxValue, StringSplitOptions.None });
+
+      Console.WriteLine("Fruits: ");
+      if (result is string[])
+      {
+        foreach (var item in result as string[])
+        {
+          Console.WriteLine(item);
+        }
+      }
+    }
+  }
+}
