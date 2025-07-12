f1e70d4bc5846fe3536f640e3baacaa1cba41866
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-07-11 22:41:18 -0300

    077_reflection_methods_of_system_string

---
+++ 077_reflection_methods_of_system_string/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+
+// We first obtain type information on string
+Type typeInfoOnString = typeof(string);
+
+// The array contains MethodInfo objects, each of which has information on the methods of 
+// a string type
+MethodInfo[] stringMethods = typeInfoOnString.GetMethods();
+
+Console.WriteLine($"{typeInfoOnString.Name} has these methods:");
+
+// We iterate the array and print out the information regarding the method
+foreach (var methodInfo in stringMethods)
+{
+  ParameterInfo[] parameterInfos = methodInfo.GetParameters();
+
+  string parameters = ParameterInfosToString(parameterInfos);
+
+  Console.WriteLine($"{methodInfo.Name}({parameters}) -> returns {methodInfo.ReturnType}.");
+}
+
+
+// This method gives the list of method parameters in a readable form
+static string ParameterInfosToString(ParameterInfo[] parameterInfos)
+{
+  string parameters = "";
+
+  for (int i = 0; i < parameterInfos.Length - 1; i++)
+  {
+    parameters += $"{parameterInfos[i].ParameterType.Name} {parameterInfos[i].Name}, ";
+  }
+
+  if (parameterInfos.Length > 0)
+  {
+    int lastIndex = parameterInfos.Length - 1;
+    parameters += $"{parameterInfos[lastIndex].ParameterType.Name} {parameterInfos[lastIndex].Name}";
+  }
+
+  return parameters;
+
+}
