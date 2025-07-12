db0c700b865998d5880e03c45b5f8ee17f863432
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-07-11 22:41:36 -0300

    079_reflection_private_fields_system_string

---
+++ 079_reflection_private_fields_system_string/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reflection;
+
+Type typeInfoOnString = typeof(string);
+var privateInstanceFields = typeInfoOnString.GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+
+Console.WriteLine($"{typeInfoOnString.Name} has these private instance fields:");
+
+foreach (var fieldInfo in privateInstanceFields)
+{
+  Console.WriteLine($"{fieldInfo.Name}");
+}
