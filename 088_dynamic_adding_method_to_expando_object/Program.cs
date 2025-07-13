676bfde1843510a65fe9298cc5ee591f30fb44bd
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-07-13 18:28:56 -0300

    088_dynamic_adding_method_to_expando_object

---
+++ 088_dynamic_adding_method_to_expando_object/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Dynamic;
+
+dynamic car = new ExpandoObject();
+
+car.Model = "Corolla";
+car.Price = 15000;
+car.Color = "Blue";
+
+Console.WriteLine($"Model: {car.Model}");
+Console.WriteLine($"Price: ${car.Price}");
+Console.WriteLine($"Color: {car.Color}");
+
+// Create a property of type Action (which is essentially a method)
+car.Accelerate = new Action(() => { Console.WriteLine("Accelerating..."); });
+
+// Call the delegate
+car.Accelerate();
