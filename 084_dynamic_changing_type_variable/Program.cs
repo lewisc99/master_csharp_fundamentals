74417b71ec463b75432056f7562e58067707913d
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-07-13 18:12:10 -0300

    084_dynamic_changing_type_variable

---
+++ 084_dynamic_changing_type_variable/Program.cs
@@ -0,0 +1,10 @@
+﻿// We assign an int value in the beginning
+dynamic someVariable = 24;
+
+Console.WriteLine($"Type of someVariable: {someVariable.GetType()}, value: {someVariable}.");
+
+// Can assign string
+someVariable = "Now, it's a string";
+
+Console.WriteLine($"Type of someVariable: {someVariable.GetType()}, value: {someVariable}");
+
