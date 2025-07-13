064cb881cf8d0f6ba1c2877c738d25216dccb251
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-07-13 18:12:01 -0300

    083_dynamic_runtime_error_due_non_existing_property

---
+++ 083_dynamic_runtime_error_due_non_existing_property/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+// We declare the variable using the dynamic keyword
+dynamic course = new Course() { Title = ".NET Fundamentals" };
+
+// Program compiles and runs because there is no compile-time checking
+// for dynamic objects
+Console.WriteLine("Program  started running");
+
+// However, because there is no Duration property,
+// The program fails during runtime
+Console.WriteLine(course.Duration);
