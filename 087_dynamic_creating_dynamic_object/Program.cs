6a1b7423b1aebb0ab75e771e7ac4977c86ca67ea
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-07-13 18:28:49 -0300

    087_dynamic_creating_dynamic_object

---
+++ 087_dynamic_creating_dynamic_object/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Dynamic;
+
+// We instatiate the ExpandoObject
+dynamic car = new ExpandoObject();
+
+// Add members
+car.Model = "Corolla";
+car.Price = 1500;
+car.Color = "Blue";
+
+// User the members
+Console.WriteLine($"Model: {car.Model}");
+Console.WriteLine($"Price: ${car.Price}");
+Console.WriteLine($"Color: {car.Color}");
