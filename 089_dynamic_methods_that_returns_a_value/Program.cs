3b4d1889ceb828c2e936c43fdd7d1a1437745303
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-07-13 18:29:04 -0300

    089_dynamic_methods_that_returns_a_value

---
+++ 089_dynamic_methods_that_returns_a_value/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Dynamic;
+
+namespace DynamicLanguageRuntime
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      dynamic car = new ExpandoObject();
+
+      car.Model = "Corolla";
+      car.Price = 15000;
+      car.Color = "Blue";
+
+      Console.WriteLine($"Model: {car.Model}");
+      Console.WriteLine($"Price: ${car.Price}");
+      Console.WriteLine($"Color: {car.Color}");
+
+      car.Accelerate = new Action(() => { Console.WriteLine("Accelerating..."); });
+
+      car.Accelerate();
+
+      // The delegate returns an integer value
+      car.GetFuelEconomy = new Func<int>(() => { return 30; });
+
+      // Call the delegate and store the result in a variable
+      int corollaFuelEconomy = car.GetFuelEconomy();
+
+      Console.WriteLine($"Corolla's range is {corollaFuelEconomy} miles per gallon.");
+    }
+  }
+}
