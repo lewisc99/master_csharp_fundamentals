e49e36ebc5a1d49da3ddbc9e5083bca9e11dda1e
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-07-17 22:28:14 -0300

    093_garbage_collector_robust_disposing

---
+++ 093_garbage_collector_robust_disposing/Course.cs
@@ -0,0 +1,19 @@
+using System;
+
+
+// Implement the IDisposable interface
+public class Course : IDisposable
+{
+  public string Title { get; set; }
+
+  ~Course()
+  {
+    Console.WriteLine($"Course object with title {Title} claimed by the garbage collector.");
+  }
+
+  // We implement this method
+  public void Dispose()
+  {
+    Console.WriteLine("Releasing unmanaged resources...");
+  }
+}
---
+++ 093_garbage_collector_robust_disposing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+Course course = null;
+
+try
+{
+  // All the work with the object goes inside the try block
+  course = new Course() { Title = ".NET Fundamentals" };
+
+  Console.WriteLine($"Course title: {course.Title}");
+}
+finally
+{
+  // In this block, we first check if the object is not null
+  // and then call the Dispose() method
+  course?.Dispose();
+}
