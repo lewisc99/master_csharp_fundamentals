37e09a9f9a3cc3c3f30f09e13984dd84edde9cb9
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-07-18 22:01:38 -0300

    097_garbage_collector_extending_the_scode_using_block

---
+++ 097_garbage_collector_extending_the_scode_using_block/Program.cs
@@ -0,0 +1,8 @@
+﻿using var course = new Course() { Title = ".NET Fundamentals" };
+
+Console.WriteLine($"Course object with title {course.Title} is created.");
+
+// Now, the whole Main() is the scope of of the using construct.
+// We can access the course variable
+Console.WriteLine(course.Title);
+
