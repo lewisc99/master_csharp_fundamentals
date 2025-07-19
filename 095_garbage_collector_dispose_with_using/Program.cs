3ddecff93be7f385269a3a512cb45ec4b0e9e4b3
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-07-18 22:01:15 -0300

    095_garbage_collector_dispose_with_using

---
+++ 095_garbage_collector_dispose_with_using/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using (var course = new Course() { Title = ".NET Fundamentals" })
+{
+  Console.WriteLine($"Course object with title {course.Title} is created.");
+  // Dispose() is called when existing the block
+}
+
+Console.WriteLine("End of Main()");
