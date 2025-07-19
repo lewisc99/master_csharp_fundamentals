38d067298721648a477346584c2269aba01a4cca
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-07-18 22:01:27 -0300

    096_garbage_collector_using_block_are_not_reachable

---
+++ 096_garbage_collector_using_block_are_not_reachable/Course.cs
@@ -0,0 +1,37 @@
+using System;
+
+public class Course : IDisposable
+{
+  private bool disposed = false;
+
+  public string Title { get; set; }
+
+  public void Dispose()
+  {
+    Dispose(true);
+
+    GC.SuppressFinalize(this);
+  }
+
+  protected virtual void Dispose(bool disposing)
+  {
+    if (disposed)
+    {
+      return;
+    }
+
+    if (disposing)
+    {
+      Console.WriteLine("Releasing managed resources.");
+    }
+
+    Console.WriteLine("Releasing unmanaged resources.");
+
+    disposed = true;
+  }
+
+  ~Course()
+  {
+    Dispose(false);
+  }
+}
---
+++ 096_garbage_collector_using_block_are_not_reachable/Program.cs
@@ -0,0 +1,7 @@
+﻿using (var course = new Course() { Title = ".NET Fundamentals" })
+{
+  Console.WriteLine($"Course object with title {course.Title} is created.");
+}
+
+// We can't reach the objects created in the using block
+Console.WriteLine(course.Title);
