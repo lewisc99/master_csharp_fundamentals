e3bc798c868f230b6b6fd6c83a5440784d53dcc1
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-07-23 22:20:03 -0300

    098_garbage_collector_listing_files

---
+++ 098_garbage_collector_listing_files/Program.cs
@@ -0,0 +1,12 @@
+﻿// Gets the current directory where the app executable is located
+string currentDirectory = Directory.GetCurrentDirectory();
+
+// Gets the list of files
+string[] files = Directory.GetFiles(currentDirectory);
+
+Console.WriteLine("Files in current directory:");
+
+foreach (var file in files)
+{
+  Console.WriteLine(file);
+}
