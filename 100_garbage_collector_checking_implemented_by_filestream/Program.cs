2502965f613c2ea269471c695d4c1b57e1782c28
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-07-23 22:20:27 -0300

    100_garbage_collector_checking_implemented_by_filestream

---
+++ 100_garbage_collector_checking_implemented_by_filestream/Program.cs
@@ -0,0 +1,8 @@
+﻿Type fileStreamTypeInfo = typeof(FileStream);
+
+Type[] implementedInterfaces = fileStreamTypeInfo.GetInterfaces();
+
+foreach (var iface in implementedInterfaces)
+{
+  Console.WriteLine(iface.Name);
+}
