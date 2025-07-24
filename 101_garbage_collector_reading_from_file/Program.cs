2d1c4a6bc9800fd66c2b1aa6c91f242b3b9ca4aa
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-07-23 22:20:35 -0300

    101_garbage_collector_reading_from_file

---
+++ 101_garbage_collector_reading_from_file/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Text;
+using (var fileStream = new FileStream("Program.cs", FileMode.Open))
+{
+  byte[] byteSequence = new byte[fileStream.Length];
+  fileStream.Read(byteSequence, 0, byteSequence.Length);
+  string text = Encoding.UTF8.GetString(byteSequence);
+
+  Console.WriteLine(text);
+} // Dispose() is calling at the end
